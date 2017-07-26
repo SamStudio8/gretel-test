b0ebfda96a22378ab10edd2d9e755ffad07ad8dc
Robert Edwards <raedwards@gmail.com>
2017-07-26 12:03:02 +1000

    Adding argparser and some arguments

--- snpper.py
+++ snpper.py
@@ -6,15 +6,22 @@ import numpy as np
 import pysam
 import argparse
 
-parser = argparse.ArgumentParser('Aggressively call for variants and generate a VCF')
+parser = argparse.ArgumentParser('Aggressively call for variants and generate a VCF', epilog='NOTE: Coordinates are 1-based as they are for samtools')
 parser.add_argument('-b', help='bam file', required=True)
 parser.add_argument('-r', help='reference sequence (contig) to match', required=True)
-parser.add_argument('-s', help='start', type=int, default=0)
-parser.add_argument('-e', help='end', type=int)
+parser.add_argument('-s', help='start (default = 1)', type=int, default=1)
+parser.add_argument('-e', help='end (default = length of the reference)', type=int)
 parser.add_argument('-d', help='minimum depth to call (default = 0)', type=int, default=0)
 args = parser.parse_args()
 
 bam = pysam.AlignmentFile(args.b)
+
+# convert 1-indexed numbers to 0 indexed numbers for pysam
+if not args.e:
+    args.e = bam.lengths[bam.references.index(args.r)] - 1
+
+args.s = args.s - 1
+
 counts = np.array(bam.count_coverage(reference=args.r, start=args.s, end=args.e, quality_threshold=0, read_callback='nofilter'))
 
 COUNT_SENSITIVITY = args.d
